91795a6d4faf42e8fe92fe4cca0339f0f78ff80e
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-16 20:11:07 +0900

    2021 03 16 - name-> algorithm_tag

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, send_file
 from BaekJoon import get_Algorithm
 
 app = Flask("Algorithm_Search")
-
+app.run(host="0.0.0.0")
 db = {}
 
 
@@ -13,18 +13,20 @@ def home_page():
 
 @app.route("/report")
 def report():
-    word = request.args.get('word')
-    if word:
-        word = word.lower()
-        # db에서 찾았던 알고리즘인지 확인.
-        existing_Algorithm = db.get(word)
-        # 있는 경우.
-        if existing_Algorithm:
-            Algorithm = existing_Algorithm
-        # 없는 경우.
-        else:
-            Algorithm = get_Algorithm(word)
-            db[word] = Algorithm
-    else:
-        return redirect("/")
-    return render_template("report.html", searchingBy=word, resultsNumber=len(Algorithm), Algorithm=Algorithm)
+    # 알고리즘 태그이름 가져옴...
+    Algorithm__tag = request.args.get('Algorithm__tag')
+    # if Algorithm__tag:
+    #     # Algorithm__tag = word.lower()
+    #     # db에서 찾았던 알고리즘인지 확인.
+    #     existing_Algorithm = db.get(Algorithm__tag)
+    #     # 있는 경우.
+    #     if existing_Algorithm:
+    #         Algorithm = existing_Algorithm
+    #     # 없는 경우.
+    #     else:
+    #         Algorithm = get_Algorithm(Algorithm__tag)
+    #         db[Algorithm__tag] = Algorithm
+    # else:
+    #     return redirect("/")
+    return render_template("report.html", searchingBy=Algorithm__tag)
+    # , resultsNumber=len(Algorithm), Algorithm=Algorithm)
