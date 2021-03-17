dd3115e57bfe4288c3ebf589c2c6b53bab04d4e4
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-17 19:10:09 +0900

    acmic_problem search

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from flask import Flask, render_template, request, redirect, send_file
-from BaekJoon import get_Algorithm
+from flask import Flask, render_template,request,redirect
+from acmic import getAlgorithmTag
+app = Flask("Algo")
 
-app = Flask("Algorithm_Search")
-app.run(host="0.0.0.0")
-db = {}
-
+db={}
 
 @app.route("/")
-def home_page():
-    return render_template("home_page.html")
-
+def homepage():
+  return render_template("Search.html")
 
 @app.route("/report")
 def report():
-    # 알고리즘 태그이름 가져옴...
-    Algorithm__tag = request.args.get('Algorithm__tag')
-    # if Algorithm__tag:
-    #     # Algorithm__tag = word.lower()
-    #     # db에서 찾았던 알고리즘인지 확인.
-    #     existing_Algorithm = db.get(Algorithm__tag)
-    #     # 있는 경우.
-    #     if existing_Algorithm:
-    #         Algorithm = existing_Algorithm
-    #     # 없는 경우.
-    #     else:
-    #         Algorithm = get_Algorithm(Algorithm__tag)
-    #         db[Algorithm__tag] = Algorithm
-    # else:
-    #     return redirect("/")
-    return render_template("report.html", searchingBy=Algorithm__tag)
-    # , resultsNumber=len(Algorithm), Algorithm=Algorithm)
+  Algorithm__tag=request.args.get("Algorithm__tag")
+  if Algorithm__tag:
+    Algorithm__tag=Algorithm__tag.lower()
+    # Tag=getAlgorithmTag(Algorithm__tag)
+    existingTag=db.get(Algorithm__tag)
+    if existingTag:
+      problems=existingTag
+    else :
+      problems=getAlgorithmTag(Algorithm__tag)
+      db[Algorithm__tag]=problems  
+  else :
+    return redirect("/")
+  return render_template("report.html",searchingBy=Algorithm__tag,resultsNumber=len(problems),problems=problems)
+
+app.run(host="0.0.0.0")
